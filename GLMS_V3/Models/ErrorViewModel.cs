6e763530c018ed16417a728a1fe7be31d8225007
HalaMzobe <HalaMzobe@HALAMZ>
2026-06-17 00:50:20 +0200

    Add project files.

---
+++ GLMS_V3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GLMS_V3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
