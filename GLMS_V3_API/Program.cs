5300552e0ac4f686ae8af177b0999a4fe81ae50a
HalaMzobe <HalaMzobe@HALAMZ>
2026-06-17 20:47:44 +0200

    Add Docker Compose support and configure API Swagger for container environment

--- GLMS_V3_API/Program.cs
+++ GLMS_V3_API/Program.cs
@@ -55,11 +55,13 @@ builder.Services.AddHttpClient<CurrencyService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+////if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
